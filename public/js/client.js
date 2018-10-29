0e6efa2dd699ed7db53deb186157dff244ec2d32
me@m3ns1.com <tuaid4GitHub>
2018-10-29 17:16:47 +0100

    - NPE fix in sorters

--- public/js/client.js
+++ public/js/client.js
@@ -119,8 +119,8 @@ function sortOnPagina(articleController, t, opts, sort) {
         function (lhs_card, rhs_card) {
             let lhs = articleController.getByCard(lhs_card);
             let rhs = articleController.getByCard(rhs_card);
-            let lhsp = parseInt(lhs.pagina || "0");
-            let rhsp = parseInt(rhs.pagina || "0");
+            let lhsp = lhs !== null ? parseInt(lhs.pagina || "0") : 1;
+            let rhsp = rhs !== null ? parseInt(rhs.pagina || "0") : 1;
             if (lhsp > rhsp) {
                 return sort === "asc" ? 1 : -1;
             } else if (rhsp > lhsp) {
@@ -151,8 +151,8 @@ function sortOnTags(articleController, t, opts, sort) {
         function (lhs_card, rhs_card) {
             let lhs = articleController.getByCard(lhs_card);
             let rhs = articleController.getByCard(rhs_card);
-            let lhsp = map(lhs.tags);
-            let rhsp = map(rhs.tags);
+            let lhsp = lhs !== null && lhs.tags !== null ? map(lhs.tags) : 1;
+            let rhsp = rhs !== null && rhs.tags !== null ? map(rhs.tags) : 1;
             if (lhsp > rhsp) {
                 return sort === "asc" ? 1 : -1;
             } else if (rhsp > lhsp) {
