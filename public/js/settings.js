202b36032a786728093a886da4a80ba3581f9ac7
me@m3ns1.com <tuaid4GitHub>
2018-11-25 08:36:21 +0100

    - Version kann aktuell noch nicht dynamisch geladen werden

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,6 +1,7 @@
 t.render(function () {
 
     let stag = document.createElement('script');
+    // TODO this is somehow not yet working because on sandbox the /version resource cannot be created somehow
     stag.setAttribute('src', '/version.jsonp');
     stag.async = true;
     stag.addEventListener('load', function (e) {
