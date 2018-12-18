f81c047531e93dc060e9a96b230fb0c197b872de
me@m3ns1.com <tuaid4GitHub>
2018-12-18 06:50:15 +0100

    - New Grid System

--- public/js/helpers/templates.js
+++ public/js/helpers/templates.js
@@ -3,25 +3,25 @@ let template_regular = '<div id="template">'+
     '    <div class="row">'+
     '        <div class="col-6">'+
     '            <div class="row">'+
-    '                <div class="col-12 less-padding-right">'+
+    '                <div class="col-12">'+
     '                    <div class="pa.name"></div>'+
     '                </div>'+
-    '                <div class="col-12 less-padding-right">'+
+    '                <div class="col-12">'+
     '                    <div class="pa.social"></div>'+
     '                </div>'+
-    '                <div class="col-12 less-padding-right">'+
+    '                <div class="col-12">'+
     '                    <div class="pa.address"></div>'+
     '                </div>'+
     '            </div>'+
     '        </div>'+
     '        <div class="col-6">'+
-    '            <div class="row">'+
-    '                <div class="col-12 less-padding-left before-last-row">'+
+    '            <div class="row line-6">'+
+    '                <div class="col-12">'+
     '                    <div class="pa.notes"></div>'+
     '                </div>'+
     '            </div>'+
     '            <div class="row">'+
-    '                <div class="col-12 less-padding-left align-bottom">'+
+    '                <div class="col-12">'+
     '                    <div class="pa.duedate"></div>'+
     '                </div>'+
     '            </div>'+
@@ -30,16 +30,16 @@ let template_regular = '<div id="template">'+
     '    <div class="row">'+
     '        <div class="col-12">'+
     '            <div class="row">'+
-    '                <div class="col-3 less-padding-right">'+
+    '                <div class="col-3">'+
     '                    <div class="pa.fee"></div>'+
     '                </div>'+
-    '                <div class="col-3 less-padding">'+
+    '                <div class="col-3">'+
     '                    <div class="pa.charges"></div>'+
     '                </div>'+
-    '                <div class="col-3 less-padding">'+
+    '                <div class="col-3">'+
     '                    <div class="pa.project"></div>'+
     '                </div>'+
-    '                <div class="col-3 less-padding-left">'+
+    '                <div class="col-3">'+
     '                    <div class="pa.cap_on_expenses"></div>'+
     '                </div>'+
     '            </div>'+
@@ -49,40 +49,39 @@ let template_regular = '<div id="template">'+
 
 let template_ad = '<div id="template" class="row">'+
     '    <div class="col-6">'+
-    '        <div class="row">'+
-    '            <div class="col-12 less-padding-right">'+
+    '        <div class="row ">'+
+    '            <div class="col-12">'+
     '                <div class="pa.notes"></div>'+
     '            </div>'+
     '        </div>'+
-    '        <div class="row before-last-row">'+
-    '            <div class="col-6 less-padding-right">'+
+    '        <div class="row">'+
+    '            <div class="col-6">'+
     '                <div class="pa.format"></div>'+
     '            </div>'+
-    '            <div class="col-6 less-padding">'+
+    '            <div class="col-6">'+
     '                <div class="pa.placement"></div>'+
     '            </div>'+
     '        </div>'+
-    '        <div class="row align-bottom">'+
-    '            <div class="col-6 less-padding-right">'+
+    '        <div class="row">'+
+    '            <div class="col-6">'+
     '                <div class="pa.price"></div>'+
     '            </div>'+
-    '            <div class="col-6 less-padding">'+
+    '            <div class="col-6">'+
     '                <div class="pa.total"></div>'+
     '            </div>'+
     '        </div>'+
     '    </div>'+
     '    <div class="col-6">'+
     '        <div class="row">'+
-    '            <div class="col-12 less-padding-left">'+
+    '            <div class="col-12">'+
     '                <div class="pa.name"></div>'+
     '            </div>'+
-    '            <div class="col-12 less-padding-left">'+
+    '            <div class="col-12">'+
     '                <div class="pa.social"></div>'+
     '            </div>'+
-    '            <div class="col-12 less-padding-left">'+
+    '            <div class="col-12">'+
     '                <div class="pa.address"></div>'+
     '            </div>'+
     '        </div>'+
     '    </div>'+
     '</div>';
-
